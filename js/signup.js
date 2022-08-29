c6c1ca8aa7d106cd051304e44a3484e361582dd3
Gabriel Adams Bogar <gabeab34@gmail.com>
2022-08-29 15:18:57 -0400

    changed name to username

--- js/signup.js
+++ js/signup.js
@@ -1,18 +1,14 @@
 const signupFormHandler = async (event) => {
     event.preventDefault();
   
-    const name = document.querySelector("#name-signup").value;
+    const username = document.querySelector("#username-signup").value;
     const password = document.querySelector("#password-signup").value;
   
-    if (name && password) {
+    if (username && password) {
       const response = await fetch("api/users/signup", {
         method: "POST",
         body: JSON.stringify({
-          name,
-          tag,
-          email,
-          phone,
-          region,
+          username,
           password,
         }),
         headers: { "Content-Type": "application/json" },
