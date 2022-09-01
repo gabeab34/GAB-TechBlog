fb87197f3240e7509085a702458d999a67f71272
Gabriel Adams Bogar <gabeab34@gmail.com>
2022-09-01 15:27:29 -0400

    site up at localhost

---
+++ controllers/api/index.js
@@ -0,0 +1,9 @@
+const router = require("express").Router();
+// const userRoutes = require("./user_route.js");
+// const eventRoutes = require("./event_route.js");
+// const event_routes = require("./event_route.js");
+
+// router.use("/users", userRoutes);
+// router.use("/event", event_routes);
+
+module.exports = router;
